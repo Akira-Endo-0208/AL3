0dfa413d4a686a99c604a98dad9ccfc8f2b36085
AKIRA_ENDO_0208 <k021g1166@g.neec.ac.jp>
2022-04-13 13:10:27 +0900

    CG2の確認

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow("LE2B_08_エンドウ_アキラ_AL3");
+	win->CreateGameWindow("LE2B_08_エンドウ_アキラ_CG2");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
