a184a035082d37e51c378a98c58e57650ecf4553
AKIRA_ENDO_0208 <k021g1166@g.neec.ac.jp>
2022-04-14 14:29:58 +0900

    ワールドの変換行列

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -6,17 +6,44 @@ using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() 
+{
+	delete model_;
+}
 
-void GameScene::Initialize() {
+
+void GameScene::Initialize() 
+{
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+	textureHandle_ = TextureManager::Load("mario.jpg");
+	model_ = Model::Create();
+
+	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
+	worldTransform_.rotation_ = {XM_PI / 4.0f, XM_PI / 4.0f, 0.0f};
+	worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
 }
 
-void GameScene::Update() {}
+void GameScene::Update() 
+{ 
+	debugText_->SetPos(40, 50);
+	debugText_->Printf(
+	  "translation:(%f,%f,%f)", worldTransform_.translation_.x, worldTransform_.translation_.y,
+	  worldTransform_.translation_.z);
+	debugText_->SetPos(40, 70);
+	debugText_->Printf(
+	  "translation:(%f,%f,%f)", worldTransform_.rotation_.x, worldTransform_.rotation_.y,
+	  worldTransform_.rotation_.z);
+	debugText_->SetPos(40, 90);
+	debugText_->Printf(
+	  "translation:(%f,%f,%f)", worldTransform_.scale_.x, worldTransform_.scale_.y,
+	  worldTransform_.scale_.z);
+}
 
 void GameScene::Draw() {
 
@@ -44,9 +71,10 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
+
 #pragma endregion
 
 #pragma region 前景スプライト描画
@@ -65,3 +93,4 @@ void GameScene::Draw() {
 
 #pragma endregion
 }
+
