9f872b63c490cfe66cd4252f97a2710f3ef6601f
AKIRA_ENDO_0208 <k021g1166@g.neec.ac.jp>
2022-06-13 09:36:22 +0900

    re_02_01_basic

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -1,16 +1,16 @@
 ﻿#pragma once
 
 #include "Audio.h"
-#include "DirectXCommon.h"
+#include "DebugCamera.h"
 #include "DebugText.h"
+#include "DirectXCommon.h"
 #include "Input.h"
 #include "Model.h"
 #include "SafeDelete.h"
 #include "Sprite.h"
+#include "TransferWorldMatrix.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
-#include "DebugCamera.h"
-#include "TransferWorldMatrix.h"
 #include <DirectXMath.h>
 
 using namespace WorldMat;
@@ -47,9 +47,6 @@ class GameScene {
 	/// </summary>
 	void Draw();
 
-	
-
-
   private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
@@ -58,10 +55,64 @@ class GameScene {
 	uint32_t textureHandle_ = 0;
 	Model* model_ = nullptr;
 	DebugCamera* debugCamera_ = nullptr;
-	WorldTransform worldTransforms_[100];
+	WorldTransform worldTransform_;
 	ViewProjection viewProjection_;
 
-	float viewAngle = 0.0f;
+	Vector3 zLine[20] = {
+	  {-20.0f, 0.0f, -20.0f},
+      {-20.0f, 0.0f, 20.0f },
+      {-15.0f, 0.0f, -20.0f},
+      {-15.0f, 0.0f, 20.0f },
+	  {-10.0f, 0.0f, -20.0f},
+      {-10.0f, 0.0f, 20.0f },
+      {-5.0f,  0.0f, -20.0f},
+      {-5.0f,  0.0f, 20.0f },
+	  {0.0f,   0.0f, -20.0f},
+      {0.0f,   0.0f, 20.0f },
+      {5.0f,   0.0f, -20.0f},
+      {5.0f,   0.0f, 20.0f },
+	  {10.0f,  0.0f, -20.0f},
+      {10.0f,  0.0f, 20.0f },
+      {15.0f,  0.0f, -20.0f},
+      {15.0f,  0.0f, 20.0f },
+	  {20.0f,  0.0f, -20.0f},
+      {20.0f,  0.0f, 20.0f }
+    };
+
+	Vector3 xLine[20] = {
+	  {-20.0f, 0.0f, -20.0f},
+      {20.0f,  0.0f, -20.0f},
+      {-20.0f, 0.0f, -15.0f},
+      {20.0f,  0.0f, -15.0f},
+	  {-20.0f, 0.0f, -10.0f},
+      {20.0f,  0.0f, -10.0f},
+      {-20.0f, 0.0f, -5.0f },
+      {20.0f,  0.0f, -5.0f },
+	  {-20.0f, 0.0f, 0.0f  },
+      {20.0f,  0.0f, 0.0f  },
+      {-20.0f, 0.0f, 5.0f  },
+      {20.0f,  0.0f, 5.0f  },
+	  {-20.0f, 0.0f, 10.0f },
+      {20.0f,  0.0f, 10.0f },
+      {-20.0f, 0.0f, 15.0f },
+      {20.0f,  0.0f, 15.0f },
+	  {-20.0f, 0.0f, 20.0f },
+      {20.0f,  0.0f, 20.0f }
+    };
+
+	int edgeList[18][2] = {
+	  {0,  1 },
+      {2,  3 },
+      {4,  5 },
+      {6,  7 },
+      {8,  9 },
+      {10, 11},
+      {12, 13},
+      {14, 15},
+      {16, 17},
+	};
+	Vector4 blue;
+	Vector4 red;
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
